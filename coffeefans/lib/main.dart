a706951b316105b5e00615fab46dfe920630fc2f
YUSSIF ABDUL-RAHAMAN <abdulrahamanyussif62@gmail.com>
2024-08-29 08:15:13 +0100

    Update main.dart

--- coffeefans/lib/main.dart
+++ coffeefans/lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Hello, Man'),
-  ));
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Coffe Fan'),
+      backgroundColor: Colors.brown[700],
+      centerTitle: true,
+    ),
+    body: const Text('Welcome back Coffee fan'),
+  )));
 }
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      width: 200,
+      height: 100,
+      padding: const EdgeInsets.all(30),
+      margin: EdgeInsets.only(left: 20),
+      child: const Text('Hello Coffe fan',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.5,
+          )),
+    );
+  }
+}
+
+//Container, this wraps other widgets and it takes all the available space around it automatically
+
